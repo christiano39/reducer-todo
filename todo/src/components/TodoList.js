3f0a2c2d8452d5855dfede5037eba198fc817969
christiano39 <carneson39@gmail.com>
2020-07-13 13:31:51 -0600

    reducer fully implemented

--- todo/src/components/TodoList.js
+++ todo/src/components/TodoList.js
@@ -4,6 +4,16 @@ import { reducer, initialState } from '../reducers/todoReducer';
 const TodoList = props => {
     const [newTodoText, setNewTodoText] = useState('');
     const [state, dispatch] = useReducer(reducer, initialState);
+
+    const handleChanges = e => {
+        setNewTodoText(e.target.value);
+    }
+
+    const onSubmit = e => {
+        e.preventDefault();
+        dispatch({ type: 'ADD_TODO', payload: newTodoText })
+        setNewTodoText('');
+    }
     
     return (
         <div className='container'>
@@ -11,11 +21,20 @@ const TodoList = props => {
                 <ul>
                     {
                         state.todoList.map(todo => {
-                            return <li key={todo.id}>{todo.item}</li>
+                            return <li key={todo.id} onClick={() => dispatch({ type: 'TOGGLE_COMPLETED', payload: todo })} className={todo.completed ? 'completed' : ''}>{todo.item}</li>
                         })
                     }
                 </ul>
             </div>
+            <form onSubmit={onSubmit}>
+                <input 
+                    type='text'
+                    value={newTodoText}
+                    onChange={handleChanges}
+                />&nbsp;
+                <button>Add</button>
+            </form>
+            <button onClick={() => dispatch({ type: 'CLEAR_COMPLETED' })}>Clear completed</button>
         </div>
     )
 }
--- todo/src/reducers/todoReducer.js
+++ todo/src/reducers/todoReducer.js
@@ -8,6 +8,42 @@ export const initialState = {
 
 export const reducer = (state, action) => {
     switch (action.type) {
+        case 'ADD_TODO':
+            if (action.payload !== ''){
+                return {
+                    ...state,
+                    todoList: [
+                        ...state.todoList,
+                        {
+                            item: action.payload,
+                            completed: false,
+                            id: new Date()
+                        }
+                    ]
+                }
+            }
+            return state
+        case 'TOGGLE_COMPLETED':
+            return {
+                ...state,
+                todoList: state.todoList.map(itm => {
+                    if (itm.id === action.payload.id){
+                        return {
+                            ...itm,
+                            completed: !itm.completed 
+                        }
+                    }else {
+                        return itm
+                    }
+                })
+            }
+        case 'CLEAR_COMPLETED':
+            return {
+                ...state,
+                todoList: state.todoList.filter(itm => {
+                    return !itm.completed
+                })
+            }
         default:
             return state
     }
